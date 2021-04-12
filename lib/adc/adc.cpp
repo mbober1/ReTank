e94f3edc38b2fb7a30aa3ca3a5597d39390bf9dd
Marcin Bober <mbober1@gmail.com>
2021-04-12 18:14:59 +0200

    adc percentage calculation

--- lib/adc/adc.cpp
+++ lib/adc/adc.cpp
@@ -11,3 +11,11 @@ float myADC::getVoltage() {
     for (int i = 0; i < 1000; i++) adc_reading += adc1_get_raw(ADC1_CHANNEL_0);
     return ((float)adc_reading / 1000) * this->conversion_factor;
 }
+
+int myADC::getPercentage() {
+    float voltage = this->getVoltage();
+    int percentage = (voltage - 11.1)*66;
+    if(percentage < 0) return 0;
+    else if(percentage > 100) return 100;
+    return percentage;
+}
